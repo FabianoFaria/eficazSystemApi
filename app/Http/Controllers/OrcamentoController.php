b8c10283330103361067208115d9dfe50939de0e
Fabiano Hatori <sistemaeficaz@sistema.eficazsystem.com.br>
2017-10-31 10:27:25 -0200

    Correcao de query

--- app/Http/Controllers/OrcamentoController.php
+++ app/Http/Controllers/OrcamentoController.php
@@ -249,6 +249,7 @@ class OrcamentoController extends Controller
                                     DB::raw('SUM(opp.Quantidade * opp.Valor_Venda_Unitario) as Valor_Total_Proposta'),
                                     DB::raw('count(opp.Proposta_Produto_ID) as Total_Itens_Proposta'),
                                     'opv.Data_Vencimento',
+                                    'opv.Dias_Vencimento',
                                     'opv.Dias_Vencimento'
                                 )
                                 ->where([
@@ -262,6 +263,7 @@ class OrcamentoController extends Controller
                                     'op.Titulo',
                                     'op.Proposta_ID',
                                     'opv.Data_Vencimento',
+                                    'opv.Dias_Vencimento',
                                     'opv.Dias_Vencimento'
                                 )
                                 ->get();
